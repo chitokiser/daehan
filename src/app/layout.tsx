9617f270871d73c7110935948a08b312a0af015e
daguri <daguri75@gmail.com>
2026-09-11 13:23:33 +0900

    fix: Update favicon icons and html head links for all devices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,6 +43,9 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <head>
+        <link rel="icon" href="/images/favicon.png" type="image/png" />
+        <link rel="shortcut icon" href="/images/favicon.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/images/favicon.png" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
